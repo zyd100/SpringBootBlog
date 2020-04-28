f2b182f18513d0ed87d2ea5fbd3be13b078b76d8
zyd100 <1019661738@qq.com>
2020-04-28 19:20:45 +0800

    feat: 删除项目前端代码 开始着手设计前端项目以及前端接口

--- SpringBootBlog/src/main/java/com/zyd/blog/web/ArticleController.java
+++ SpringBootBlog/src/main/java/com/zyd/blog/web/ArticleController.java
@@ -6,6 +6,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import com.zyd.blog.util.ResultFactory;
 import cn.hutool.core.util.StrUtil;
 import io.swagger.annotations.Api;
 
+
 @Api("ArticleController")
 @RestController
 @RequestMapping(path = "/articles", produces = {"application/json; charset=utf-8"})
@@ -25,7 +27,7 @@ public class ArticleController {
 
   @Autowired
   private ArticleService articleService;
-
+  
 
   @PostMapping
   @PreAuthorize("hasRole('ROLE_ADMIN')")
@@ -36,6 +38,15 @@ public class ArticleController {
     return ResultFactory.generateSuccessResult(article);
   }
 
+  @PutMapping
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  public Result<Article> updateArticle(@RequestBody Article article) {
+    article.setUpdateTime(LocalDateTime.now());
+    article.setSummary(StrUtil.sub(article.getContent(), 0, 100));
+    articleService.update(article);
+    return ResultFactory.generateSuccessResult(article);
+  }
+  
   @GetMapping("/{id}")
   public Result<ArticleDto> getArticle(@PathVariable int id) {
     return ResultFactory.generateSuccessResult(articleService.findById(id));
@@ -45,5 +56,5 @@ public class ArticleController {
   public Result<PageInfo<Article>> getArticleSummary(int pageNum, int pageSize) {
     return ResultFactory.generateSuccessResult(articleService.findAllSummary(pageNum, pageSize));
   }
-  
+
 }
--- SpringBootBlog/src/main/java/com/zyd/blog/web/UserInfoController.java
+++ SpringBootBlog/src/main/java/com/zyd/blog/web/UserInfoController.java
@@ -3,8 +3,10 @@ package com.zyd.blog.web;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +33,17 @@ public class UserInfoController {
   public Result<Object>saveInfo(@RequestBody UserInfo userInfo){
     userInfoService.save(userInfo);
     return ResultFactory.generateSuccessResult(userInfo);
+  }
+  @PutMapping
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  public Result<Object>updateInfo(@RequestBody UserInfo userInfo){
+    userInfoService.update(userInfo);
+    return ResultFactory.generateSuccessResult(userInfo);
+  }
+  @DeleteMapping
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  public Result<Object>deleteInfo(@RequestBody UserInfo userInfo){
+    userInfoService.deleteById(userInfo.getId());
+    return ResultFactory.generateSuccessResult("删除成功");
   }
 }
